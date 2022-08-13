e049b7040229f299b24fa40322656ffdbf18fc7c
pokaneprogrammer <mishenko.s80@mail.ru>
2022-08-14 02:37:46 +0300

    Урок2Задача1

---
+++ Урок2Задача1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+System.Console.WriteLine();
+int number = new Random().Next(100, 200);
+
+System.Console.WriteLine($"Второй цифрой числа {number} является {number / 10 % 10} ");
