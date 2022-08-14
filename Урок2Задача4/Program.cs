900b976abb1d99fcda1389aef1889e43ae537a29
pokaneprogrammer <mishenko.s80@mail.ru>
2022-08-14 03:23:53 +0300

    Урок2Задача4

---
+++ Урок2Задача4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Clear();
+System.Console.WriteLine();
+
+int dayNumber = new Random().Next(1, 7);
+string[] Days = { "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье" };
+string txtResult = "";
+
+if (dayNumber > 5) txtResult = $". Выходной день";
+else txtResult = $", рабочий день.";
+
+System.Console.WriteLine();
+System.Console.WriteLine($"{dayNumber} день недели - это {Days[dayNumber - 1]}{txtResult}");
+System.Console.WriteLine();
