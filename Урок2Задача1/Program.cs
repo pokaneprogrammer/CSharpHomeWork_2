60957037cfd448c86c561a600af3881b8f8e657d
pokaneprogrammer <109076841+pokaneprogrammer@users.noreply.github.com>
2022-08-14 03:26:54 +0300

    Update Program.cs

--- Урок2Задача1/Program.cs
+++ Урок2Задача1/Program.cs
@@ -4,3 +4,4 @@ System.Console.WriteLine();
 int number = new Random().Next(100, 200);
 
 System.Console.WriteLine($"Второй цифрой числа {number} является {number / 10 % 10} ");
+System.Console.WriteLine(); 
